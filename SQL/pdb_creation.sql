7ed38862935d22aa35fd878c50f3f654062e0c23
MBISHIBISHI Flavien <flavmbish@gmail.com>
2025-10-05 01:11:51 +0200

    Create pdb_creation.sql

---
+++ SQL/pdb_creation.sql
@@ -0,0 +1,3 @@
+CREATE PLUGGABLE DATABASE fl_pdb_27857
+    ADMIN USER flavien_plsqlauca_27857 IDENTIFIED BY 1234
+    FILE_NAME_CONVERT = ('/u01/app/oracle/oradata/CDB1/pdbseed/','/u01/app/oracle/oradata/CDB1/fl_pdb_27857/');
